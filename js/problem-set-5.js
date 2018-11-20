dd7452f28de88eb18189bade27823a4967803b58
Ironcrabman <43757458+Ironcrabman@users.noreply.github.com>
2018-11-20 14:10:55 -0500

    Update problem-set-5.js

--- js/problem-set-5.js
+++ js/problem-set-5.js
@@ -28,8 +28,8 @@ function mario() {
 
   // WRITE YOUR EXERCISE 1 CODE HERE
 height = prompt("Please Enter an integer number between 1 and 23");
- if(height > 23 || height < 1 || height == NaN) {
-  while(height > 23 || height < 1 || height || height == NaN){
+ if(height > 23 || height < 1 || height === NaN) {
+  while(height > 23 || height < 1 || height === NaN){
   height = prompt("Please enter in a VALID whole integer between 1 and 23");
   }
  }
